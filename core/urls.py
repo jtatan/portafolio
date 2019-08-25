6ecdaeb54e97b8785330661f4b6e692f4f9632b4
tt <jorge.tatan@gmail.com>
2019-08-25 10:08:38 -0400

    Configurando Media y Staticfiles

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index')
-]
+    path('', IndexView.as_view(), name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
