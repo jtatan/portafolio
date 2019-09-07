48718f56a9a2beb86461f909022754b66044debb
tt <jorge.tatan@gmail.com>
2019-09-07 18:54:17 -0400

    Template y enlaces para Categorías (VistaDetalle). Listar Artículos por Categoría.

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ArticuloVista, ArticuloDetalleVista
+from .views import ArticuloListaVista, ArticuloDetalleVista, CategoriaListaVista, CategoriaDetalleListaVista
 from . import views
 
 
 urlpatterns = [
-    path('labs/', ArticuloVista.as_view(), name='lista'),
-    path('labs/<slug>/', ArticuloDetalleVista.as_view(), name='detalle')
+    path('labs/', ArticuloListaVista.as_view(), name='lista'),
+    path('labs/<slug>/', ArticuloDetalleVista.as_view(), name='detalle'),
+    path('categorias/', CategoriaListaVista.as_view(), name='categoria-lista'),
+    path('categorias/<slug>/', CategoriaDetalleListaVista.as_view(), name='categoria-detalle')
 ]
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from .models import Articulo
+from .models import Articulo, Categoria
 
-
-class ArticuloVista(ListView):
+# ----- Artículos ----- #
+class ArticuloListaVista(ListView):
     model = Articulo
     template_name = 'labs.html'
     context_object_name = 'articulos'
@@ -11,7 +11,6 @@ class ArticuloVista(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-
 
 class ArticuloDetalleVista(DetailView):
     template_name = 'labs/articulos.html'
@@ -21,3 +20,28 @@ class ArticuloDetalleVista(DetailView):
     def get_object(self):
         slug_ = self.kwargs.get("slug")
         return get_object_or_404(Articulo, slug=slug_)
+
+# ----- Categorías ----- #
+
+class CategoriaListaVista(ListView):
+    model = Categoria
+    template_name = 'labs/categorias/lista.html'
+    context_object_name = 'categorias'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class CategoriaDetalleListaVista(DetailView):
+    template_name = 'labs/categorias/detalle.html'
+    queryset = Categoria.objects.all()
+    context_object_name = 'categoria'
+
+    def get_object(self):
+        slug_ = self.kwargs.get("slug")
+        return get_object_or_404(Categoria, slug=slug_)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoriaDetalleListaVista, self).get_context_data(**kwargs)
+        context['articulos'] = Articulo.objects.filter(categorias=self.get_object())
+        return context
